dc97a59c9f3811002a0d8978f19c13d92b9364a5
nabil-moh <132611734+nabil-moh@users.noreply.github.com>
2026-09-06 11:43:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-{
-  "appId": "com.hayati.store",
-  "appName": "Hayati",
-  "webDir": "www"
-}
+window.HAYATI_CONFIG={
+  SUPABASE_URL:'https://hgzxgpzvblbtuxtebykv.supabase.co',
+  SUPABASE_ANON_KEY:'sb_publishable_SnCexjyZ9Osz8SB5A7z32g_hE0KWpTm'
+};
